786f884e177e294fd71dfa96dd81eb58164d3c31
BoneFabric <iburanguloff@ya.ru>
2022-02-20 07:37:27 +0500

    bootstrap 5

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,2 +1,20 @@
-<a href="{{ url('logout') }}">Log out</a>
-admin index
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<div class="container">
+    <a href="{{ url('logout') }}">Log out</a>
+    admin index
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
