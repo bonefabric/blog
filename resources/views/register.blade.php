c1e14e9b224333f5673e73ee8a74c5bc4ef4e153
BoneFabric <iburanguloff@ya.ru>
2022-02-21 17:13:44 +0500

    register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,35 +1,45 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<style>
-    form {
-        display: flex;
-        flex-direction: column;
-        width: 300px;
-        margin-top: 200px;
-        margin-left: 50%;
-        transform: translateX(-50%);
-    }
-</style>
-<body>
-<form action="{{ url('/register') }}" method="post">
-    @csrf
-    <input type="text" name="name" placeholder="Name"/>
-    <input type="email" name="email" placeholder="E-mail"/>
-    <input type="password" name="password" placeholder="Password"/>
-    <input type="password" name="password_confirmation" placeholder="Confirm password"/>
-    <input type="submit">
+@extends('templates.main')
 
-    @foreach($errors->all() as $error)
-        <p>{{ $error }}</p>
-    @endforeach
-</form>
+@section('header')
+    @include('templates.header')
+@endsection
 
-</body>
-</html>
+@section('body')
+    <div class="row mt-5">
+        <div class="col-6 offset-3">
+            <form action="{{ url('/register') }}" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="inputName" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="inputName" name="name">
+                </div>
+                <div class="mb-3">
+                    <label for="inputEmail" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" name="email">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="inputPassword" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" name="password">
+                </div>
+                <div class="mb-3">
+                    <label for="inputPasswordConfirmation" class="form-label">Confirm password</label>
+                    <input type="password" class="form-control" id="inputPasswordConfirmation" name="password_confirmation">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+    @if($errors->any())
+        <div class="row mt-5">
+            <div class="col-8 offset-2">
+                @foreach($errors->all() as $error)
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ $error }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    @endif
+@endsection
