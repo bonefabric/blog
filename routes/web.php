78da57a8602b570e18110f4b1555266ad5ed3b04
BoneFabric <iburanguloff@ya.ru>
2022-02-21 13:50:58 +0500

    route names

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,32 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\PostsController;
 use App\Http\Controllers\Admin\TagsController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Profile\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::view('/', 'index')->name('home');
 
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/login', [AuthController::class, 'loginView'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
 
-    Route::get('/register', [AuthController::class, 'registerView']);
+    Route::get('/register', [AuthController::class, 'registerView'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 });
 
+Route::group(['middleware' => 'auth'], function () {
 
-Route::group(['middleware' => 'auth', 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 
-    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::group(['middleware' => 'auth.admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
 
-    Route::resource('posts', PostsController::class);
-    Route::resource('tags', TagsController::class);
+        Route::get('/', [AdminController::class, 'index'])->name('index');
+
+        Route::resource('posts', PostsController::class);
+        Route::resource('tags', TagsController::class);
+
+    });
 
 });
