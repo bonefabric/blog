3d48220f91ea2563a0bbab848c55d4737501c874
BoneFabric <iburanguloff@ya.ru>
2022-02-21 13:51:16 +0500

    profile page

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.main')
+
+@section('header')
+    @include('templates.header')
+@endsection
+
+@section('body')
+    <div class="profile"></div>
+@endsection
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -7,3 +7,43 @@
         </div>
     @endif
 @endauth
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Blog</a>
+            </li>
+        </ul>
+        <div class="d-flex">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle{{ Route::current()->named('profile') ? ' active' : '' }}" href="{{ route('profile') }}"
+                           id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Profile
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ route('profile') }}">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="{{ route('logout') }}">Log out</a></li>
+                        </ul>
+                    </li>
+                @endauth
+                @guest
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Account
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ route('login') }}">Log in</a></li>
+                            <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
+                        </ul>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
