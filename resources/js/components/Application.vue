5071056daa01c0cacbf821d7c41cdea80772191f
BoneFabric <iburanguloff@ya.ru>
2022-02-25 10:22:05 +0500

    main template

--- resources/js/components/Application.vue
+++ resources/js/components/Application.vue
@@ -1,15 +1,25 @@
 <template>
-    <router-view/>
+    <router-view v-if="!loading"/>
+    <span v-else>Loading...</span>
 </template>
 
 <script>
 import {defineComponent} from "vue";
-import {Profile} from "../api/Profile";
 
 export default defineComponent({
     name: "Application",
-    mounted() {
-        Profile.authorize();
+    data() {
+        return {
+            loading: true,
+        }
+    },
+    beforeMount() {
+        this.$store.dispatch('init').finally(() => {
+            this.loading = false;
+            if (!this.$store.state.profile.isAuthorized) {
+                this.$router.push({ name: 'login' });
+            }
+        });
     }
 })
 </script>
