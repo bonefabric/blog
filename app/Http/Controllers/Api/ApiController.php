e5be649644a13ab1cffd9f1e7acf8b0f3a87e4bd
BoneFabric <iburanguloff@ya.ru>
2022-02-25 09:32:07 +0500

    main api controller

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class ApiController extends Controller
+{
+
+    /**
+     * @param array $data
+     * @param int $status
+     * @return JsonResponse
+     */
+    public function sendResponse(array $data = [], int $status = 200): JsonResponse
+    {
+        return response()->json(
+            array_merge($data, [
+                'meta' => [
+                    'status' => $status,
+                ],
+            ])
+        )->send();
+    }
+}
