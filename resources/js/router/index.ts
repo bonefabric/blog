4b1cff90b11727c3231e9a42afac226c937c7cc9
BoneFabric <iburanguloff@ya.ru>
2022-02-26 12:19:17 +0500

    router to typescript

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -1,82 +1,91 @@
-import {createRouter, createWebHistory} from "vue-router";
-import Index from "../components/views";
+import {
+    createRouter,
+    createWebHistory, NavigationGuardNext,
+    RouteLocationNormalized, RouteLocationRaw, RouteMeta,
+    Router,
+    RouteRecordRaw
+} from "vue-router";
 import {AUTH} from "../config";
 import store from "../store";
+import Index from "../components/views/Index";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     {
         name: 'index',
         path: '/',
         component: Index,
         meta: {
             access: AUTH.ALL,
-        }
-    },
+        } as RouteMeta,
+    } as RouteRecordRaw,
     {
         name: 'login',
         path: '/login',
         component: () => import("../components/views/Login"),
         meta: {
             access: AUTH.GUEST,
-        }
-    },
+        } as RouteMeta,
+    } as RouteRecordRaw,
     {
         name: 'register',
         path: '/register',
         component: () => import("../components/views/Register"),
         meta: {
             access: AUTH.GUEST,
-        }
-    },
+        } as RouteMeta,
+    } as RouteRecordRaw,
     {
         name: 'about',
         path: '/about',
         component: () => import("../components/views/About"),
         meta: {
             access: AUTH.ALL,
-        }
-    },
+        } as RouteMeta,
+    } as RouteRecordRaw,
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     linkActiveClass: 'active',
     routes,
-});
+}) as Router;
 
-router.beforeEach((to, from, next) => {
-    const accessedTo = router.checkAccess(to);
+export const checkAccess = function (route: RouteLocationNormalized) {
+    if (!route.meta) {
+        return null;
+    }
+    switch (route.meta.access) {
+        case AUTH.ALL:
+            return null;
+        case AUTH.GUEST:
+            if (store.state.profile.isAuthorized) {
+                return {name: 'index'} as RouteLocationRaw;
+            }
+            return null;
+        case AUTH.USER:
+            if (!store.state.profile.isAuthorized) {
+                return {name: 'login'} as RouteLocationRaw;
+            } else if (store.state.profile.isAuthorized && store.state.profile.isBanned) {
+                return {name: 'index'} as RouteLocationRaw;
+            }
+            return null;
+        case AUTH.ADMIN:
+            if (!store.state.profile.isAuthorized || !store.state.profile.isAdmin) {
+                return {name: 'index'} as RouteLocationRaw;
+            }
+            return null;
+        default:
+            return {name: 'index'} as RouteLocationRaw;
+    }
+};
+
+router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext): void => {
+    const accessedTo = checkAccess(to);
     if (accessedTo) {
         next(accessedTo);
         return;
     }
     next();
 });
-
-router.checkAccess = function (route) {
-    switch (route.meta.access) {
-        case AUTH.ALL:
-            return null;
-        case AUTH.GUEST:
-            if (store.state.profile.isAuthorized) {
-                return {name: 'index'};
-            }
-            return null;
-        case AUTH.USER:
-            if (!store.state.profile.isAuthorized) {
-                return {name: 'login'};
-            } else if (store.state.profile.isAuthorized && store.state.profile.isBanned) {
-                return {name: 'index'};
-            }
-            return null;
-        case AUTH.ADMIN:
-            if (!store.state.profile.isAuthorized || !store.state.profile.isAdmin) {
-                return {name: 'index'};
-            }
-            return null;
-        default:
-            return {name: 'index'};
-    }
-};
 
 export default router;
